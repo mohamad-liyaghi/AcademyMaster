8a18b3577c7185396e07fb0349ee71285a7d6334
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-06-07 12:57:08 +0330

    Pytest configs added

--- backend/config/settings/test.py
+++ backend/config/settings/test.py
@@ -4,14 +4,13 @@ from .core import *
 DEBUG = True
 ALLOWED_HOSTS = list(config('ALLOWED_HOSTS'))
 
-# TODO Add test db for tests
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT')
+        'NAME': config('TEST_DATABASE_NAME'),
+        'USER': config('TEST_DATABASE_USER'),
+        'PASSWORD': config('TEST_DATABASE_PASSWORD'),
+        'HOST': config('TEST_DATABASE_HOST'),
+        'PORT': config('TEST_DATABASE_PORT')
     }
 }
