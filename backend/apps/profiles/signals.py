3c1e5d554d7a13e0edf69e918d6fe2886e1af8e4
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-06-13 13:15:43 +0330

    Profile docs added

--- backend/apps/profiles/signals.py
+++ backend/apps/profiles/signals.py
@@ -6,7 +6,7 @@ from profiles.models import Profile
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_profile(sender, instance, created, **kwargs):
+def create_profile_for_active_user(sender, instance, created, **kwargs):
     '''Create profile for a user as soon as it is activated'''
     if instance.is_active:
         profile = Profile.objects.filter(user=instance)
