47b98ac1bd74978a94f876d7b407624ba1384878
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-06-15 16:10:04 +0330

    Send Promotion mail added

--- backend/apps/managers/apps.py
+++ backend/apps/managers/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ManagersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'managers'
+
+    def ready(self) -> None:
+        import managers.signals
