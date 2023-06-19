597e7d8aacea63e954a73f0ba38070c7198808cf
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-06-19 14:06:23 +0330

    Send teacher promotion mail signal added

--- backend/apps/managers/signals.py
+++ backend/apps/managers/signals.py
@@ -6,17 +6,22 @@ from apps.core.tasks import send_email
 
 
 @receiver(post_save, sender=Manager)
-def send_promotion_email(sender, created, instance, **kwargs):
+def send_manager_promotion_email(sender, created, instance, **kwargs):
     if created:
         manager = instance
         user = manager.user
+        promoter_full_name = (
+            instance.promoted_by.full_name
+            if instance.promoted_by else None
+        )
 
         send_email.delay(
-            template_path="emails/notify_promotion.html",
+            template_path="emails/notify_manager_promotion.html",
             to_email=user.email,
             subject="You've been promoted to Manager!",
             context={
-                'verification_code': manager.promotion_date,
+                'promotion_date': manager.promotion_date,
+                'promoter': promoter_full_name,
                 'first_name': user.first_name,
                 'manager_token': manager.token
             }
---
+++ backend/apps/teachers/signals.py
@@ -0,0 +1,29 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+from teachers.models import Teacher
+from apps.core.tasks import send_email
+
+
+@receiver(post_save, sender=Teacher)
+def send_teacher_promotion_email(sender, created, instance, **kwargs):
+    '''Send an email to user when promoted as teacher'''
+    if created:
+        instance = instance
+        user = instance.user
+        promoter_full_name = (
+            instance.promoted_by.full_name
+            if instance.promoted_by else None
+        )
+
+        send_email.delay(
+            template_path="emails/notify_teacher_promotion.html",
+            to_email=user.email,
+            subject="You've been promoted to Teacher!",
+            context={
+                'promoter': promoter_full_name,
+                'promotion_date': instance.promotion_date,
+                'first_name': user.first_name,
+                'manager_token': instance.token
+            }
+        )
