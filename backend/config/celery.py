908566553aaacaa5a8ad2a7e977eab874e378928
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-06-26 15:58:41 +0330

    Test DB connection fixed

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -1,8 +1,5 @@
-import os
 from celery import Celery
 from decouple import config
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 
 celery = Celery('config', broker=config("CELERY_BROKER_URL"))
 celery.config_from_object('django.conf:settings', namespace='CELERY')
