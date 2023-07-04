8dcd03b6f669567882769125fa21f082f06a94fa
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-07-04 14:07:52 +0330

    Activity Model added

--- backend/apps/activities/admin.py
+++ backend/apps/activities/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Activity
 
-# Register your models here.
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ['user', 'course', 'token']
--- backend/apps/activities/models.py
+++ backend/apps/activities/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.conf import settings
+from core.models import AbstractToken
+from courses.models import Course
+from enrollments.models import Enrollment
 
-# Create your models here.
+
+class Activity(AbstractToken):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='activities'
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='activities'
+    )
+
+    enrollment = models.ForeignKey(
+        Enrollment,
+        on_delete=models.CASCADE,
+        related_name='activities'
+    )
+
+    attendance = models.JSONField(blank=True, null=True)
+    final_mark = models.PositiveIntegerField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['course', 'user']
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.course = self.enrollment.course
+
+        return super().save(*args, **kwargs)
