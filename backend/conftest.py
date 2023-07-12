f69fe9c026ce67611a308c61eae6697def6dead2
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-07-12 15:15:45 +0330

    Activity retrieve link added to activity creation email content

--- backend/conftest.py
+++ backend/conftest.py
@@ -9,9 +9,9 @@ from config.celery import celery
 import pytest
 
 
-@pytest.fixture(autouse=True)
-def disable_celery_tasks():
-    '''Do not execute celery tasks during tests'''
-    celery.conf.CELERY_ALWAYS_EAGER = True
-    yield
-    celery.conf.CELERY_ALWAYS_EAGER = False
+# @pytest.fixture(autouse=True)
+# def disable_celery_tasks():
+#     '''Do not execute celery tasks during tests'''
+#     celery.conf.CELERY_ALWAYS_EAGER = True
+#     yield
+#     celery.conf.CELERY_ALWAYS_EAGER = False
