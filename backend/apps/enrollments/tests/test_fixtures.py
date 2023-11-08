bd6919801c90ef8986ed0a94b750036d271be206
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-11-08 15:37:19 +0330

    updated: enrollment tests

--- backend/apps/enrollments/tests/test_fixtures.py
+++ backend/apps/enrollments/tests/test_fixtures.py
@@ -4,10 +4,9 @@ from enrollments.models import Enrollment, EnrollmentStatus
 
 @pytest.mark.django_db
 def test_create_enrollment(create_enrollment):
-    assert Enrollment.objects.count() == 1
     assert create_enrollment.status == EnrollmentStatus.PENDING
+
 
 @pytest.mark.django_db
 def test_create_success_enrollment(create_success_enrollment):
-    assert Enrollment.objects.count() == 1
     assert create_success_enrollment.status == EnrollmentStatus.SUCCESS
